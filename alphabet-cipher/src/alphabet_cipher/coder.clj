16cadfc78af114b4f38d4adc2c684cc3c3b86e84
Arul Prakash Pugazhendi <spv.arul@gmail.com>
2016-11-26 15:45:50 +0530

    Solution completed for alphabet cipher

--- alphabet-cipher/src/alphabet_cipher/coder.clj
+++ alphabet-cipher/src/alphabet_cipher/coder.clj
@@ -1,11 +1,31 @@
 (ns alphabet-cipher.coder)
 
+(def letters (vec (map str '[a b c d e f g h i j k l m n o p q r s t u v w x y z])))
+
+(defn get-codes
+  [letter]
+  (let [number (.indexOf letters letter)]
+    (vec (flatten (conj (take number letters) (drop number letters))))))
+
+(defn encode-letter
+  [k m]
+  (get (get-codes k) (.indexOf letters m)))
+
+(defn decode-letter
+  [k m]
+  (get letters (.indexOf (get-codes k) m)))
+
 (defn encode [keyword message]
-  "encodeme")
+  (let [kseq (cycle (map str (vec keyword)))
+        mseq (map str (vec message))]
+    (reduce str (map encode-letter kseq mseq))))
 
 (defn decode [keyword message]
-  "decodeme")
+  (let [kseq (cycle (map str (vec keyword)))
+        mseq (map str (vec message))]
+    (reduce str (map decode-letter kseq mseq))))
 
 (defn decipher [cipher message]
-  "decypherme")
+  (let [kw (decode message cipher)]
+    (reduce str (take (first (filter #(= (take % kw) (take % (drop % kw))) (range 1 (count kw)))) kw))))
 
